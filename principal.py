0493a9c8abdbc3586ed2d4c293878e0137aa310d
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-10-24 17:28:20 +0000

    Add função inss

---
+++ principal.py
@@ -0,0 +1,14 @@
+#git branch
+#git checkout feature-funcao-inss (criando funcionalidade)
+#git checkout -b fix-funcao-inss (corrigir o problema)
+#
+
+import os
+
+os.system("cls || clear")
+
+print("Adicionando funcionalidade: função inss.")
+
+def inss():
+    # considere feito.
+    pass
