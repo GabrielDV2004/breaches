5ffa13e2dd45cc94ceca35049f93407703ee4c85
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-10-24 17:36:43 +0000

    24/10

--- principal.py
+++ principal.py
@@ -1,7 +1,8 @@
 #git branch
 #git checkout feature-funcao-inss (criando funcionalidade)
 #git checkout -b fix-funcao-inss (corrigir o problema)
-#
+#git checkout main
+#git checkout feature-funcao-inss
 
 import os
 
