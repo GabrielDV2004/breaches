80304f3d5385ca5b8b64a5edb82985f276b4e68a
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-10-24 19:12:05 +0000

    salvar

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+# git branch (verifica qual breanch voce tem ou esta utilizando)
+# git checkout -b feature-funcao-inss (criando funcionalidade)
+# git checkout -b fix-funcao-inss (corrigir o problema)
+# git checkout main
+# git checkount feature-funcao-inss
+# git branch -d feature-funcao-inss or mais (apaga o branch)
+# git log ( verifica o historico de commits)
+# git merge feature-salario-liquido
+#
+#
+#
 print("Construindo uma aplicação.")
 
 def salario_liquido():
     pass
 
 def salario_bruto():
+    pass
+
+def corrigindo_fix():
     pass
 
 
